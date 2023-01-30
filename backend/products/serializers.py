155d2baaf0055d30154d55975f6f563797aa0d91
omkar_kabade <94742654+Calibrill@users.noreply.github.com>
2023-01-30 20:54:10 +0530

    part-10-modelSerializers

---
+++ backend/products/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField(read_only = True) # renaming the property name which is in models.py
+    class Meta:
+        model = Product
+        fields = ['title','content','price','sale_price','discount',]
+    
+    def get_discount(self,obj):
+        return obj.get_discount() #calling property and returning it here
